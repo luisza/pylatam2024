90ac2746147e58bdf050a641739d226026afd87f
Luis Zárate Montero <luis.zarate@solvosoft.com>
2024-07-23 10:33:40 -0600

    Fist commit

---
+++ src/pycon/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+urlpatterns = [
+    path('', views.home, {'lang': 'es'}, name="home"),
+    path('es/', views.home, {'lang': 'es'}, name="home_es"),
+    path('en/', views.home, {'lang': 'en'}, name="home_en")
+]
---
+++ src/pycon/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.utils.translation import activate
+
+
+# Create your views here.
+
+def home(request, lang='es'):
+    activate(lang)
+    return render(request, 'home.html')
