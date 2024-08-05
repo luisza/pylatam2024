d156c125d03a32b6cc8edc282f3979323d77f060
Luis Zárate Montero <luis.zarate@solvosoft.com>
2024-08-04 21:42:50 -0600

    Small fixes

--- src/pylatam2024/settings.py
+++ src/pylatam2024/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3uj+l7mu1q*_krs!@#8*px)ht$k9371zgoko)sw=gvqnzjq%g+')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3uj+l7mu1q*_krs!@#8*px)ht$k9371zgoko)sw=gvqnzjq%g+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
